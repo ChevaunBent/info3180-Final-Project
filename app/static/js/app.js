57748d6f274990f17f20c493e0eb7bd8c10749ff
Vaun <Vaun@Chevaun.local>
2021-04-23 10:01:43 -0500

    further adjustments to home page

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -16,31 +16,35 @@ app.component('app-header', {
   template:
     /*html*/
     `
-      <header>
-          <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-            <a class="navbar-brand" href="#">INFO3180-Final Project</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
-            </button>
-
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
+    <header>
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+        <a class="navbar-brand" href="#">INFO3180-Final Project</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
             <li class="nav-item active">
-            <router-link to="/" class="nav-link">Home</router-link>
+              <router-link to="/" class="nav-link">Home</router-link>
             </li>
             <li class="nav-item">
-            <router-link to="/register" class="nav-link">Register</router-link>
+              <router-link to="/explore" class="nav-link">Explore</router-link>
             </li>
             <li class="nav-item">
-            <router-link to="/explore" class="nav-link">Explore</router-link>
+              <router-link to="/newcar" class="nav-link">Add Car</router-link>
+            </li>
+          </ul>
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <router-link to="/register" class="nav-link">Register</router-link>
             </li>
             <li class="nav-item">
-            <router-link to="/newcar" class="nav-link">Add Car</router-link>
+              <router-link to="/login" class="nav-link">Login</router-link>
             </li>
-           </ul>
-            </div>
-          </nav>
-      </header>    
+          </ul>
+        </div>
+      </nav>
+    </header>    
   `,
   data: function () {
     return {};
